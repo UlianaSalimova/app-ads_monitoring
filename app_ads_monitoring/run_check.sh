aa2b2c8a03e98d214ff4d7c44ffa0d7d6bedb9f9
Ульяна <144013883+UlianaSalimova@users.noreply.github.com>
2026-05-12 20:57:30 +0300

    Delete app_ads_monitoring directory

--- app_ads_monitoring/run_check.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /Users/ulboh/PycharmProjects/app_ads_monitoring
-source .venv/bin/activate
-/Users/ulboh/PycharmProjects/app_ads_monitoring/.venv/bin/python3 -c "from app.main import main; main()"
